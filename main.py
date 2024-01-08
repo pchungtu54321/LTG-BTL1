9a92ab5e2f9c6b9c5d46132c1c0f81e8ecd8ef24
ChungTus <pchungtu@gmail.com>
2024-01-08 18:48:29 +0700

    them sound

--- main.py
+++ main.py
@@ -110,3 +110,37 @@ while running:
     draw_enemies()
     show_score(textX, textY)
     pygame.display.update()
+
+class SoundEffect:
+    def __init__(self):
+        self.mainTrack = pygame.mixer.music.load("sounds/themesong.wav")
+        self.fireSound = pygame.mixer.Sound("sounds/fire.wav")
+        self.fireSound.set_volume(1.0)
+        self.popSound = pygame.mixer.Sound("sounds/pop.wav")
+        self.hurtSound = pygame.mixer.Sound("sounds/hurt.wav")
+        self.levelSound = pygame.mixer.Sound("sounds/point.wav")
+        pygame.mixer.music.play(-1)
+
+    def playFire(self):
+        self.fireSound.play()
+
+    def stopFire(self):
+        self.fireSound.sop()
+
+    def playPop(self):
+        self.popSound.play()
+
+    def stopPop(self):
+        self.popSound.stop()
+
+    def playHurt(self):
+        self.hurtSound.play()
+
+    def stopHurt(self):
+        self.hurtSound.stop()
+
+    def playLevelUp(self):
+        self.levelSound.play()
+
+    def stopLevelUp(self):
+        self.levelSound.stop()
