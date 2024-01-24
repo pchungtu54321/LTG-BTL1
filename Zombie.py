5378283a4c6f1742220ac5ced8617a90bcbb6c57
Huyshadow <huy.dang110902@gmail.com>
2024-01-24 22:31:29 +0700

    Replace done

--- Zombie.py
+++ Zombie.py
@@ -10,12 +10,15 @@ HOLE_WIDTH = 200
 HOLE_HEIGHT = 100
 ZOMBIE_MAX_HEIGHT = 100
 
+
 class ZombieState(Enum):
     GO_UP = 0
     NEED_SLAM = 1
     IS_SLAMED = 2
     GO_DOWN = 3
     NONE = 4
+
+
 class Zombie:
     def __init__(self, x, y, screen):
         self.state = ZombieState.GO_UP
@@ -35,47 +38,50 @@ class Zombie:
 
     def is_slamed(self, eventlist):
         if self.state != ZombieState.NEED_SLAM:
-            return False 
+            return False
         for event in eventlist:
             if event.type == pygame.MOUSEBUTTONUP:
                 pos = pygame.mouse.get_pos()
                 if self.x <= pos[0] and pos[0] <= self.x + ZOMBIE_WIDTH and self.y <= pos[1] and pos[1] <= self.y + ZOMBIE_HEIGHT:
                     return True
-        return False 
+        return False
 
     def change_state(self, new_state):
         self.state = new_state
 
     def draw(self):
-        surface = pygame.Surface((ZOMBIE_WIDTH,ZOMBIE_HEIGHT))
-        zombie = pygame.image.load(os.path.join(os.path.dirname(os.path.abspath('Zombie.py')), "Assets/ZOMBIE.png" 
+        surface = pygame.Surface((ZOMBIE_WIDTH, ZOMBIE_HEIGHT))
+        zombie = pygame.image.load(os.path.join(os.path.dirname(os.path.abspath('Zombie.py')), "Assets/ZOMBIE.png"
                                                 if self.state != ZombieState.IS_SLAMED else "Assets/zombie_stun.png"))
-        
-        zombie = pygame.transform.scale(zombie, (ZOMBIE_WIDTH,ZOMBIE_HEIGHT))   
+
+        zombie = pygame.transform.scale(zombie, (ZOMBIE_WIDTH, ZOMBIE_HEIGHT))
         zombie_sur = zombie.convert_alpha()
-        zombie_sur.set_alpha(self.alpha)  
-        surface.fill((255,255,255))
-        surface.set_colorkey((255,255,255))
+        zombie_sur.set_alpha(self.alpha)
+        surface.fill((255, 255, 255))
+        surface.set_colorkey((255, 255, 255))
         surface.blit(zombie_sur, (0, self.y_rise))
         if self.state == ZombieState.GO_UP:
             zombie_rect = zombie.get_rect()
             zombie_center = zombie_rect.center
-            self.screen.blit(surface, (self.x - zombie_center[0], self.y - zombie_center[1]))
+            self.screen.blit(
+                surface, (self.x - zombie_center[0], self.y - zombie_center[1]))
             self.go_up()
         if self.state == ZombieState.IS_SLAMED:
-            self.screen.blit(surface, (self.x, self.y))
+            zombie_rect = zombie.get_rect()
+            zombie_center = zombie_rect.center
+            self.screen.blit(
+                surface, (self.x - zombie_center[0], self.y - zombie_center[1]))
             self.fade()
-        
 
     def go_up(self):
         if self.y_rise == 0:
             return
-        self.y_rise -= 20 
+        self.y_rise -= 20
 
-    def fade(self): 
+    def fade(self):
         if self.alpha == 0:
             self.y_rise = ZOMBIE_MAX_HEIGHT
             self.state = ZombieState.NONE
             self.alpha = DEFAULT_ALPHA
-            return 
-        self.alpha -= 51    
+            return
+        self.alpha -= 51
